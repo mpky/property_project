3648e331e27d80fc8a8a1cf48ad37e109c044bd2
Matt P <mpokornyus@gmail.com>
2019-12-30 11:17:41 -0500

    Rename and split out processing

--- build/merge.py
+++ build/merge.py
@@ -1,6 +1,6 @@
 """
 Run this with PWD being the top level of the repo and
-python3 build/bexar_data_download_merge.py
+python build/bexar_data_download_merge.py
 
 This script will:
 1. Join the relevant columns from the TX Comptroller data file
@@ -56,50 +56,15 @@ def merge_data():
         'cleaned_name'
         ]], how='left', on='cleaned_name')
 
-    return corp_prop_merged
 
-
-def clean_add_features(corp_prop_merged):
-    # Convert SOS Charter date to datetime type
-    corp_prop_merged['SOS Charter Date'] = pd.to_datetime(corp_prop_merged['SOS Charter Date'],format='%Y%m%d')
-    # Convert deed date
-    corp_prop_merged['deed_dt'] = pd.to_datetime(corp_prop_merged['deed_dt'],format='%m%d%Y')
-
-    # Add main three features from Dutch paper
-    # Owner is a just-established company
-    # Subtract the two dates and see if the difference is within 365 days
-    corp_prop_merged['deed_charter_diff'] = corp_prop_merged['deed_dt'] - corp_prop_merged['SOS Charter Date']
-
-    # If this is <= 365, consider "just-established"
-    corp_prop_merged['just_established_owner'] = np.where(corp_prop_merged['deed_charter_diff'] <= '365 days',1,0)
-    # 16,349 properties in the dataset are considered to be owned by 'just-established' companies
-
-    # Foreign Owner
-    corp_prop_merged['foreign_based_owner'] = np.where(corp_prop_merged['py_addr_country'] != 'US',1,0)
-
-    # Unusual Price Fluctuations
-    # Determining what qualifies as "unusual" to the EDA portion, but I will calculate YoY differences for each year of data I have
-    # YoY difference from 2018 to 2019
-    corp_prop_merged['yoy_diff_2019'] = corp_prop_merged.market_value - corp_prop_merged.bexar_2018_market_value
-    # YoY difference from 2017 to 2018
-    corp_prop_merged['yoy_diff_2018'] = corp_prop_merged.bexar_2018_market_value - corp_prop_merged.bexar_2017_market_value
-    # YoY difference from 2016 to 2017
-    corp_prop_merged['yoy_diff_2017'] = corp_prop_merged.bexar_2017_market_value - corp_prop_merged.bexar_2016_market_value
-    # YoY difference from 2015 to 2016
-    corp_prop_merged['yoy_diff_2016'] = corp_prop_merged.bexar_2016_market_value - corp_prop_merged.bexar_2015_market_value
-
-    # Export as h5 file
-    print('Exporting dataframe to h5 file.')
-
-    # Mkdir
     if not os.path.exists('./data/preprocessed'):
         os.makedirs('./data/preprocessed')
 
-    corp_prop_merged.to_hdf('./data/preprocessed/bexar_merged_df.h5',
-                            key='bexar_merged_df.h5',
+    corp_prop_merged.to_hdf('./data/preprocessed/bexar_preprocessed.h5',
+                            key='bexar_preprocessed.h5',
                             mode='w'
                            )
-    print('Dataframe has been exported as bexar_merged_df.h5 to "preprocessed" data folder.')
+    print('Dataframe has been exported as bexar_preprocessed.h5 to "preprocessed" data folder.')
 
 if __name__ == '__main__':
-    clean_add_features(merge_data())
+    merge_data()
